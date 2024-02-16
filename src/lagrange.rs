738d64ee8ad4c962cbc3b6c671155a81bdae2b22
Brian Rogoff <brogoff@gmail.com>
2024-02-15 20:34:35 -0800

    Fix formatting

--- src/lagrange.rs
+++ src/lagrange.rs
@@ -1,75 +1,75 @@
 // One step in chi
 pub fn chi_step(w: bool, x: i128) -> i128 {
-	x * i128::from(w) + (1 - x) * (1 - i128::from(w))
+    x * i128::from(w) + (1 - x) * (1 - i128::from(w))
 }
 
 // Converts i into an index in {0,1}^v, used to retrieves f evaluations
 pub fn n_to_vec(i: usize, n: usize) -> Vec<bool> {
-	let x: Vec<bool> = format!("{:0>width$}", format!("{:b}", i), width = n)
-		.chars()
-		.map(|x| x == '1')
-		.collect();
-	x
+    let x: Vec<bool> = format!("{:0>width$}", format!("{:b}", i), width = n)
+        .chars()
+        .map(|x| x == '1')
+        .collect();
+    x
 }
 
 // Computes Chi_w(r) for all w, O(log n) operations
 pub fn chi_w(w: &Vec<bool>, r: &Vec<i128>) -> i128 {
-	assert_eq!(w.len(), r.len());
-	let product: i128 = w
-		.iter()
-		.zip(r.iter())
-		.map(|(&w, &r)| chi_step(w, r))
-		.product();
-	product
+    assert_eq!(w.len(), r.len());
+    let product: i128 = w
+        .iter()
+        .zip(r.iter())
+        .map(|(&w, &r)| chi_step(w, r))
+        .product();
+    product
 }
 
 // Calculating the slow way, for benchmarking
 pub fn slow_mle(fw: &Vec<i128>, r: &Vec<i128>, p: i128) -> i128 {
-	assert_eq!(r.len() as f64, (fw.len() as f64).sqrt());
-	let sum: i128 = fw
-		.iter()
-		.enumerate()
-		.map(|(i, val)| val * chi_w(&n_to_vec(i, r.len()), r))
-		.sum();
-	sum % p
+    assert_eq!(r.len() as f64, (fw.len() as f64).sqrt());
+    let sum: i128 = fw
+        .iter()
+        .enumerate()
+        .map(|(i, val)| val * chi_w(&n_to_vec(i, r.len()), r))
+        .sum();
+    sum % p
 }
 
 // Lemma 3.7
 pub fn stream_mle(fw: &Vec<i128>, r: &Vec<i128>, p: i128) -> i128 {
-	recurse(fw, r, 2usize.pow(r.len() as u32)) % p
+    recurse(fw, r, 2usize.pow(r.len() as u32)) % p
 }
 
 pub fn recurse(fw: &Vec<i128>, r: &Vec<i128>, n: usize) -> i128 {
-	match n {
-		0 => 0,
-		_ => recurse(fw, r, n - 1) + fw[n - 1] * chi_w(&n_to_vec(n - 1, r.len()), r),
-	}
+    match n {
+        0 => 0,
+        _ => recurse(fw, r, n - 1) + fw[n - 1] * chi_w(&n_to_vec(n - 1, r.len()), r),
+    }
 }
 
 // Lemm 3.8
 pub fn dynamic_mle(fw: &Vec<i128>, r: &Vec<i128>, p: i128) -> i128 {
-	let chi_lookup = memoize(r, r.len());
-	let result: i128 = fw
-		.iter()
-		.zip(chi_lookup.iter())
-		.map(|(left, right)| left * right)
-		.sum();
-	result % p
+    let chi_lookup = memoize(r, r.len());
+    let result: i128 = fw
+        .iter()
+        .zip(chi_lookup.iter())
+        .map(|(left, right)| left * right)
+        .sum();
+    result % p
 }
 
 pub fn memoize(r: &Vec<i128>, v: usize) -> Vec<i128> {
-	match v {
-		1 => {
-			vec![chi_step(false, r[v - 1]), chi_step(true, r[v - 1])]
-		}
-		_ => memoize(r, v - 1)
-			.iter()
-			.flat_map(|val| {
-				[
-					val * chi_step(false, r[v - 1]),
-					val * chi_step(true, r[v - 1]),
-				]
-			})
-			.collect(),
-	}
+    match v {
+        1 => {
+            vec![chi_step(false, r[v - 1]), chi_step(true, r[v - 1])]
+        }
+        _ => memoize(r, v - 1)
+            .iter()
+            .flat_map(|val| {
+                [
+                    val * chi_step(false, r[v - 1]),
+                    val * chi_step(true, r[v - 1]),
+                ]
+            })
+            .collect(),
+    }
 }
--- tests/test_lagrange.rs
+++ tests/test_lagrange.rs
@@ -5,18 +5,18 @@ use rstest::rstest;
 use thaler::lagrange;
 
 lazy_static! {
-	static ref F_2: Vec<i128> = Vec::from([1, 2, 1, 4]);
-	static ref R_0: Vec<i128> = Vec::from([0, 0]);
-	static ref R_1: Vec<i128> = Vec::from([0, 2]);
-	static ref R_2: Vec<i128> = Vec::from([3, 4]);
-	static ref R_3: Vec<i128> = Vec::from([4, 1]);
-	// Larger v tests
-	static ref R_4: Vec<i128> = Vec::from([0, 0, 0]);
-	static ref R_4_CHI: Vec<i128> = Vec::from([1, 0, 0, 0, 0, 0, 0, 0]);
-	static ref R_5: Vec<i128> = Vec::from([1, 2, 3]);
-	static ref R_5_CHI: Vec<i128> = Vec::from([0, 0, 0, 0, 2, -3, -4, 6]);
-	static ref R_6: Vec<i128> = Vec::from([5, 5, 5]);
-	static ref R_6_CHI: Vec<i128> = Vec::from([-64, 80, 80, -100, 80, -100, -100, 125]);
+    static ref F_2: Vec<i128> = Vec::from([1, 2, 1, 4]);
+    static ref R_0: Vec<i128> = Vec::from([0, 0]);
+    static ref R_1: Vec<i128> = Vec::from([0, 2]);
+    static ref R_2: Vec<i128> = Vec::from([3, 4]);
+    static ref R_3: Vec<i128> = Vec::from([4, 1]);
+    // Larger v tests
+    static ref R_4: Vec<i128> = Vec::from([0, 0, 0]);
+    static ref R_4_CHI: Vec<i128> = Vec::from([1, 0, 0, 0, 0, 0, 0, 0]);
+    static ref R_5: Vec<i128> = Vec::from([1, 2, 3]);
+    static ref R_5_CHI: Vec<i128> = Vec::from([0, 0, 0, 0, 2, -3, -4, 6]);
+    static ref R_6: Vec<i128> = Vec::from([5, 5, 5]);
+    static ref R_6_CHI: Vec<i128> = Vec::from([-64, 80, 80, -100, 80, -100, -100, 125]);
 }
 
 #[rstest]
@@ -25,12 +25,12 @@ lazy_static! {
 #[case(&F_2, &R_2, 4, 5)]
 #[case(&F_2, &R_3, 0, 5)]
 fn slow_lagrange_test(
-	#[case] fw: &Vec<i128>,
-	#[case] r: &Vec<i128>,
-	#[case] expected: i128,
-	#[case] p: i128,
+    #[case] fw: &Vec<i128>,
+    #[case] r: &Vec<i128>,
+    #[case] expected: i128,
+    #[case] p: i128,
 ) {
-	assert_eq!(lagrange::slow_mle(fw, r, p), expected);
+    assert_eq!(lagrange::slow_mle(fw, r, p), expected);
 }
 
 #[rstest]
@@ -39,12 +39,12 @@ fn slow_lagrange_test(
 #[case(&F_2, &R_2, 4, 5)]
 #[case(&F_2, &R_3, 0, 5)]
 fn stream_lagrange_test(
-	#[case] fw: &Vec<i128>,
-	#[case] r: &Vec<i128>,
-	#[case] expected: i128,
-	#[case] p: i128,
+    #[case] fw: &Vec<i128>,
+    #[case] r: &Vec<i128>,
+    #[case] expected: i128,
+    #[case] p: i128,
 ) {
-	assert_eq!(lagrange::stream_mle(fw, r, p), expected);
+    assert_eq!(lagrange::stream_mle(fw, r, p), expected);
 }
 
 #[rstest]
@@ -53,12 +53,12 @@ fn stream_lagrange_test(
 #[case(&F_2, &R_2, 4, 5)]
 #[case(&F_2, &R_3, 0, 5)]
 fn dynamic_mle_test(
-	#[case] fw: &Vec<i128>,
-	#[case] r: &Vec<i128>,
-	#[case] expected: i128,
-	#[case] p: i128,
+    #[case] fw: &Vec<i128>,
+    #[case] r: &Vec<i128>,
+    #[case] expected: i128,
+    #[case] p: i128,
 ) {
-	assert_eq!(lagrange::dynamic_mle(fw, r, p), expected);
+    assert_eq!(lagrange::dynamic_mle(fw, r, p), expected);
 }
 
 #[rstest]
@@ -66,5 +66,5 @@ fn dynamic_mle_test(
 #[case(&R_5,&R_5_CHI)]
 #[case(&R_6, &R_6_CHI)]
 fn memoize_test(#[case] r: &Vec<i128>, #[case] expected: &Vec<i128>) {
-	assert_eq!(lagrange::memoize(r, r.len()), *expected);
+    assert_eq!(lagrange::memoize(r, r.len()), *expected);
 }
